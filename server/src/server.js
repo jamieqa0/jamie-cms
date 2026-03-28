3e08b1a8324d9c767bf32895bd4475bf09238436
yamistar <yamistart@gmail.com>
2026-03-28 12:07:06 +0900

    chore: remove node-cron scheduler, use HTTP endpoint instead

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
-const { startScheduler } = require('./scheduler/autoDebit');
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  startScheduler();
 });
